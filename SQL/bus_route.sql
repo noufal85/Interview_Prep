8f18cc6ba283e3a20395a4319e6f0cf868ee5f80
Noufal E <noufal85@gmail.com>
2022-05-23 12:47:50 -0700

    Create bus_route.sql

---
+++ SQL/bus_route.sql
@@ -0,0 +1,27 @@
+create table destination_q
+(source_ varchar(50),
+destination_ varchar(50))
+;
+
+insert into destination_q values ('A','B');
+insert into destination_q values ('B','A');
+insert into destination_q values ('C','D');
+insert into destination_q values ('D','C');
+insert into destination_q values ('X','Y');
+insert into destination_q values ('Y','Z');
+
+commit;
+
+
+select *
+from destination_q
+
+
+select LEAST(A.source_,A.destination_)
+,coalesce(GREATEST(B.source_,B.destination_),GREATEST(A.source_,A.destination_))
+from destination_q A
+left join destination_q B
+on A.source_ = B.destination_
+and A.destination_ = B.Source_
+group by 1,2
+
